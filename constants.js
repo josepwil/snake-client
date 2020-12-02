4edef491ff7ce4574172e2763d6913081075e91c
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-02 19:47:47 +0000

    added info for lighthouse's server

--- constants.js
+++ constants.js
@@ -1,6 +1,10 @@
-const IP = '10.0.2.15';
-const PORT = 50541;
+// lighthouse's sever
+const IP = '135.23.222.131';
+const PORT = 50542;
 
+// my pc
+// const IP = '10.0.2.15';
+// const PORT = 5054;
 
 module.exports = {
   IP,
