93ece8172c19b985e0b16a5d4655654ced5378bf
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-02 16:22:53 +0000

    now listening for user input and able to quit using ctrl c

--- play.js
+++ play.js
@@ -4,3 +4,25 @@
 const  { connect } = require('./client')
 console.log('Connecting ...');
 connect();
+
+
+const setupInput = function(callback) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  // listen for key press
+  stdin.on('data',(key) => {
+    // run handleUserInput on key press
+    handleUserInput(key);
+  })
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  // if key === ctrl + c, exit the process
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+setupInput(handleUserInput);
