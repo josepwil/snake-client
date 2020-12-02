d9f79c9a4053eee3c3e85b0fb488e1a73dbbd7a1
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-02 16:46:28 +0000

    can now send movement strings to the game  server using the connection object

--- play.js
+++ play.js
@@ -5,8 +5,8 @@ const  { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
+const theConnection = connect();
 
 
 
-setupInput();
+setupInput(theConnection);
